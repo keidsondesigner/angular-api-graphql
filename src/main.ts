790b1f0b72b5b74b857c59e13069d54b387a4ecb
Keidson Roby <keidsondesigner@gmail.com>
2025-06-13 11:26:56 -0400

    feat:  Organização de imports

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,15 @@
-import { inject, LOCALE_ID } from '@angular/core';
 import { registerLocaleData } from '@angular/common';
 import { provideHttpClient } from '@angular/common/http';
 import localePtBr from '@angular/common/locales/pt';
+import { inject, LOCALE_ID } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
-import { AppComponent } from './app/app.component';
-import { routes } from './app/app.routes';
+import { InMemoryCache } from '@apollo/client/core';
 import { provideApollo } from 'apollo-angular';
 import { HttpLink } from 'apollo-angular/http';
-import { InMemoryCache } from '@apollo/client/core';
+
+import { AppComponent } from './app/app.component';
+import { routes } from './app/app.routes';
 
 // Registra os dados de localização para português do Brasil
 registerLocaleData(localePtBr, 'pt-BR');
